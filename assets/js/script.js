80ec86e61d894d81e0cd6648c8385ad4da07b25d
mickaelrutkowski <mickael.rutkowski@gmail.com>
2018-10-05 15:39:28 +0200

    correction javascript exercices 1/2

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+//exercice javascript partie 1
+/*
+declaration de la variable name
+initialise avec une chaine de carrectere (string)
+*/
+var name = "mickael";
+/*
+affichage de ma variable name dans une boite de dialogue
+avec la fonction alert()
+*/
+  alert(name);
+
+  //exercice partie 2
+
+  var firstName = "doe";
+  var lastName = "jhon";
+  var city = "amiens";
+
+//je concatène mes chaines de caractères avec les variables
+
+  alert("nom : " + firstName + "\n " + "prenom : " + lastName + "\n " + "ville : " + city);
+
+
+// var one = 2;
+// var two = 4;
+// alert(one + two);
